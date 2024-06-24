b42949e39cb58d7fd0b45de50bff7deaecbcb3ea
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-06-25 00:07:21 +0100

    add redux

--- backend-ecommerce/server.js
+++ backend-ecommerce/server.js
@@ -11,9 +11,14 @@ const io = new Server(server,{
 })
 
 
+const User = require('./models/User');
+const userRoutes = require('./routes/userRoutes');
+
+
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use('/users', userRoutes);
 
 server.listen(8080,()=>{
     console.log('server running at port',8080)
