19fa51801801c525d38b626d2d72e68a44e70f15
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-06-25 23:21:52 +0100

    modify fix hot

--- frontend-ecommerce/src/App.js
+++ frontend-ecommerce/src/App.js
@@ -6,17 +6,23 @@ import Navigation from './components/Navigation';
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from './pages/Signup';
-
+import {  useSelector } from "react-redux";
 
 function App() {
+  const user = useSelector((state) => state.user);
+
   return (
     <div className="App">
       <BrowserRouter>
       <Navigation />
       <Routes>
         <Route index element={<Home />}/>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        {!user && (
+                        <>
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/signup" element={<Signup />} />
+                        </>
+                    )}
         <Route path="*" element={<Home />}/>
       </Routes>
       </BrowserRouter>
---
+++ frontend-ecommerce/src/pages/NewProduct.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './NewProduct.css';
+
+function NewProduct() {
+  return (
+    <div>NewProduct</div>
+  )
+}
+
+export default NewProduct
