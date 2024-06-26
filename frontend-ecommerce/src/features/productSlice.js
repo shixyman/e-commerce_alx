f86fc13778f809c622282cdf80f8b5e7c0fb0152
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-06-26 23:42:54 +0100

    addproduct previews

--- frontend-ecommerce/src/features/productSlice.js
+++ frontend-ecommerce/src/features/productSlice.js
@@ -9,7 +9,9 @@ export const productSlice = createSlice({
     name: "products",
     initialState,
     reducers: {
-       
+        updateProducts: (_, action) => {
+            return action.payload;
+        },
     },
     extraReducers: (builder) => {
         builder.addMatcher(appApi.endpoints.createProduct.matchFulfilled, (_, { payload }) => payload);
--- frontend-ecommerce/src/pages/Home.js
+++ frontend-ecommerce/src/pages/Home.js
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { Link } from "react-router-dom";
 import categories from "../categories";
 import './Home.css';
-
+import axios from "../axios";
+import { useDispatch, useSelector } from "react-redux";
+import { updateProducts } from "../features/productSlice";
+import ProductPreview from "../components/ProductPreview";
 
 
 
 function Home() {
+    const dispatch = useDispatch();
+    const products = useSelector((state) => state.products);
+    const lastProducts = products.slice(0, 8);
+    useEffect(() => {
+        axios.get("/products").then(({ data }) => dispatch(updateProducts(data)));
+    }, []);
     return (
         <div>
             <img src=" https://res.cloudinary.com/learn-code-10/image/upload/v1653947013/yqajnhqf7usk56zkwqi5.png" className="home-banner" />
             <div className="featured-products-container container mt-4">
                 <h2>Last products</h2>
                 {/* last products here */}
-                
+                <div className="d-flex justify-content-center flex-wrap">
+                    {lastProducts.map((product) => (
+                        <ProductPreview {...product} />
+                    ))}
+                </div>
                 <div>
                     <Link to="/category/all" style={{ textAlign: "right", display: "block", textDecoration: "none" }}>
                         See more {">>"}
